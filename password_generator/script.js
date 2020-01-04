beca2aabec6760a2695dfd6bc44ab5f1a5c54f06
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-01-04 11:44:36 -0600

    added variables to script.js

--- password_generator/script.js
+++ password_generator/script.js
@@ -1,20 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lengthOfPassword = 0;
 
+while (lengthOfPassword <= 7 || lengthOfPassword >= 129) {
+    lengthOfPassword = prompt('How long would you like your password to be. Enter a number between 8 and 128.', '');
+    if (lengthOfPassword <= 7) {
+        alert("Must be at least 8 characters!");
+    } else if (lengthOfPassword >= 129) {
+        alert("Must be less than 128 characters!");
+    };
+}
+var capital = confirm('Would you like your password to contain capital letters?');
+
+var lowercase = confirm('Would you like your password to contain lowercase letters?');
+
+var numbers = confirm('Would you like your password to contain numbers?');
+
+var specialCharacters = confirm('Would you like your password to contain capital letters?');
+
+console.log(lengthOfPassword);
+console.log(capital);
+console.log(lowercase);
+console.log(numbers);
+console.log(specialCharacters);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+    copyBtn.removeAttribute("disabled");
+    copyBtn.focus();
 }
 
 function copyToClipboard() {
-  // BONUS 
+    // BONUS 
 }
 
 // Add event listener to generate button
